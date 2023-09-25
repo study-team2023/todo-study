c7eb062d85ee8b229ec373bf0b5a13fe2e279e03
mingming <jimin23623@gmail.com>
2023-09-25 13:50:37 +0900

    :recycle:refactor: 헤더 로그인/로그아웃 상황에 맞게 변경되도록 수정

--- todo-list/src/components/TodoHeader.js
+++ todo-list/src/components/TodoHeader.js
@@ -1,10 +1,13 @@
 import style from "../css/TodoHeader.module.css";
 import classNames from "classnames/bind";
 import {Link} from "react-router-dom";
+import { useSelector } from 'react-redux';
+import {login, logout } from "../store/loginReducer";
 
 const cn  = classNames.bind(style);
 
 const TodoHeader = () => {
+    const isLoggedIn = useSelector((state) => state.isLoggedIn);
 
     return (
         <header className={cn("header")}>
@@ -12,12 +15,27 @@ const TodoHeader = () => {
                 <h1>TodoList</h1>
             </Link>
             <div className={cn("loginWrap")}>
-                <div className={cn("'headerButtonWrap', 'login'")}>
-                    <Link to="./components/TodoLogin">로그인</Link>
-                </div>
-                <div className={cn("'headerButtonWrap', 'join'")}>
-                    <Link to="./components/TodoJoin">회원가입</Link>
-                </div>
+                {
+                    isLoggedIn ? (
+                        <>
+                        <div className={cn("'headerButtonWrap', 'login'")}>
+                            로그아웃
+                        </div>
+                        <div className={cn("'headerButtonWrap', 'join'")}>
+                            <Link to="./components/TodoProf">마이페이지</Link>
+                        </div>
+                        </>
+                    ) : (
+                        <>
+                        <div className={cn("'headerButtonWrap', 'login'")}>
+                            <Link to="./components/TodoLogin">로그인</Link>
+                        </div>
+                        <div className={cn("'headerButtonWrap', 'join'")}>
+                            <Link to="./components/TodoJoin">회원가입</Link>
+                        </div>
+                        </>
+                    )
+                }
             </div>
         </header>
     )
