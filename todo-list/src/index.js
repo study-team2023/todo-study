f58ebfcab4cdb3b12d3fc23b9f06698df75515f7
mingming <jimin23623@gmail.com>
2023-09-26 15:19:04 +0900

    :bug:fix:BrouwserRouter 코드 위치 변경

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -14,11 +14,11 @@ import { AuthProvider } from './context/AuthProvider';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+          <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
